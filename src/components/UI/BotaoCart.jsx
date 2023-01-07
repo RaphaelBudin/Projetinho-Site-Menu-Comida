070b9063b6bbe806dbd03b3f210a7081b3317dcd
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-06 23:48:28 -0300

    feat/BotaoCart criado e configurado

--- src/components/UI/BotaoCart.jsx
+++ src/components/UI/BotaoCart.jsx
@@ -3,8 +3,11 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import styles from "./BotaoCart.module.css";
 
 export default function BotaoCart(){
+    function onClickHandler(){
+        alert("Carrinho clicado");
+    }
     return(
-        <div className={styles.container}>
+        <div className={styles.container} onClick={onClickHandler}>
             <div className={styles.icone}> <FontAwesomeIcon icon={faShoppingCart} size="xs"/> </div>
             <div className={styles.texto}> Seu Carrinho</div>
             <div className={styles.contador}> 0 </div>
