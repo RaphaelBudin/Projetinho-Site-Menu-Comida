6b56338836c22d06ec2fbcf09d0d3f2af4f21540
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-07 16:46:11 -0300

    feat/Contador mockado funcionando

--- src/components/UI/BotaoCart.jsx
+++ src/components/UI/BotaoCart.jsx
@@ -2,6 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import styles from "./BotaoCart.module.css";
 
+//Dados Mockados
+const contador = Math.floor(Math.random()*100);
+
 export default function BotaoCart(){
     function onClickHandler(){
         alert("Carrinho clicado");
@@ -10,7 +13,7 @@ export default function BotaoCart(){
         <div className={styles.container} onClick={onClickHandler}>
             <div className={styles.icone}> <FontAwesomeIcon icon={faShoppingCart} size="xs"/> </div>
             <div className={styles.texto}> Seu Carrinho</div>
-            <div className={styles.contador}> 0 </div>
+            <div className={styles.contador}> {contador} </div>
         </div>
     );
 }
