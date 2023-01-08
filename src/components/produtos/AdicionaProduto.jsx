3b304ce363b4e0268f054036c8e8a132b4814fd7
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-07 22:53:31 -0300

    feat/OnClickHandler criado + useRef

--- src/components/produtos/AdicionaProduto.jsx
+++ src/components/produtos/AdicionaProduto.jsx
@@ -1,13 +1,23 @@
+import { useRef } from "react";
 import styles from "./AdicionaProduto.module.css";
 
-export default function AdicionaProduto({ addProdutoHandler }) {
+export default function AdicionaProduto({ addProdutoHandler, idProduto }) {
+  const quantidade = useRef();
+
+  function onClickHandler() {
+    addProdutoHandler(idProduto, quantidade.current.value);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.primeiraLinha}>
         <p className={styles.quantia}>Quantia</p>
-        <input type="number" className={styles.input}/>
+        <input type="number" className={styles.input} ref={quantidade} />
       </div>
-    <button onClick={addProdutoHandler} className={styles.botao}> Add + </button>
+      <button onClick={onClickHandler} className={styles.botao}>
+        {" "}
+        Add +{" "}
+      </button>
     </div>
   );
 }
