e7d5900f5e26fc0ce84e20c9522b994838dfc9d0
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-09 18:51:59 -0300

    feat/Carrinho  recebendo produtos

--- src/components/cart/Carrinho.jsx
+++ src/components/cart/Carrinho.jsx
@@ -1,6 +1,24 @@
+import { useContext } from "react";
+import {
+  AllProductsContext,
+  ProductContext,
+} from "../../context/lista-produtos-context";
 import styles from "./Carrinho.module.css";
 
-export default function Carrinho({produtosCarrinho, modalHandler}) {
+export default function Carrinho({ modalHandler }) {
+  const produtosCarrinho = useContext(ProductContext);
+  const opcoesMenu = useContext(AllProductsContext);
+
+  function retornaPropriedadeObjeto(objeto, propriedade) {
+    const produto = opcoesMenu.find((prod) => prod.id === objeto.id);
+    switch (propriedade) {
+      case "nome":      return produto.nome;
+      case "descricao": return produto.descricao;
+      case "valor":     return produto.valor;
+      default:          return "Propriedade do switch não identificada";
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -8,13 +26,16 @@ export default function Carrinho({produtosCarrinho, modalHandler}) {
         <span onClick={modalHandler}>❌</span>
       </div>
       <div className={styles.conteudo}>
-        {produtosCarrinho && produtosCarrinho.map(produto => {
-            return <div>
-                <p> {produto.nome} </p>
-                <p> {produto.descricao} </p>
-                <p> {produto.preco} </p>
-            </div>
-        })}
+        {produtosCarrinho &&
+          produtosCarrinho.map((produto) => {
+            return (
+              <div key={produto.id} value={produto}>
+                <p> {retornaPropriedadeObjeto(produto, "nome")} </p>
+                <p> {retornaPropriedadeObjeto(produto, "descricao")} </p>
+                <p> {retornaPropriedadeObjeto(produto, "valor")} </p>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
