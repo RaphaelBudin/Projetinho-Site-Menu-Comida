b07b4fd747445c84580679163f1def8542abb398
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-09 18:01:56 -0300

    feat/Produtos duplicados atualiza quantidade

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { TituloLorem, TextoLorem } from "./helper/HelperFunctions";
 import Navbar from "./components/navbar/Navbar";
 import CardCinza from "./components/UI/CardCinza";
@@ -5,17 +6,76 @@ import CardBranco from "./components/UI/CardBranco";
 import styles from "./App.module.css";
 import FotoMesa from "./public/foto-mesa.jpg";
 
+const opcoesMenu = [
+  { id: 1, nome: "Sushi", descricao: "Peixe cru, arroz e alga", valor: 22.99 },
+  { id: 2, nome: "Schitzel", descricao: "Uma iguaria Alemâ", valor: 16.77 },
+  {
+    id: 3,
+    nome: "Hamburguer",
+    descricao: "Uma das poucas comidas americanas que você conhece",
+    valor: 9.17,
+  },
+  {
+    id: 4,
+    nome: "Sopa Verde",
+    descricao: "Couve em tiras, bacon, calabresa e azeite cozidos",
+    valor: 12.75,
+  },
+];
+
 function App() {
+  const [carrinho, setCarrinho] = useState([]);
+
+  useEffect(() => {
+    console.log("Carrinho: ", carrinho);
+  }, [carrinho]);
+
   return (
     <div>
       <Navbar />
       <div className={styles.mainContainer}>
         <img src={FotoMesa} className={styles.imagemFundo} alt="" />
         <CardCinza titulo={TituloLorem} descricao={TextoLorem} />
-        <CardBranco />
+        <CardBranco addProductHandler={addProductHandler} itens={opcoesMenu} />
       </div>
     </div>
   );
+
+  function addProductHandler(produtoSelecionado, qtdProdutoSelecionado) {
+    console.log("AddProductHandler");
+    console.log("produtoSelecionado", produtoSelecionado);
+    console.log("qtdProdutoSelecionado", qtdProdutoSelecionado);
+    if (verificaJaExiste(produtoSelecionado)) {
+      console.log("PRODUTO JÁ EXISTE");
+      const arrayAtualizado = atualizaQuantidade(
+        // ...opcoesMenu,
+        produtoSelecionado,
+        qtdProdutoSelecionado
+      );
+      setCarrinho(arrayAtualizado);
+      return;
+    }
+    const novoProduto = {
+      id: produtoSelecionado,
+      quantidade: qtdProdutoSelecionado,
+    };
+    setCarrinho((prevState) => [...prevState, novoProduto]);
+  }
+
+  // Deixar idiomático: tem que receber o array via argumento
+  function verificaJaExiste(itemID) {
+    const produtoJaExiste = carrinho.find((op) => op.id === itemID);
+    return produtoJaExiste ? true : false;
+  }
+
+  // Deixar idiomático: tem que receber o array via argumento
+  function atualizaQuantidade(produtoSelecionado, quantidadeADD) {
+    const arrayAtualizado = carrinho.map((item) => {
+      if (item.id !== produtoSelecionado) return item;
+      return { id: item.id, quantidade: (+item.quantidade) + (+quantidadeADD) };
+    });
+    return arrayAtualizado;
+  }
 }
 
 export default App;
