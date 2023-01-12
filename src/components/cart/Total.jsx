f9366eb88fda3f69201ca1e3e90a1bfa30eb4abf
RaphaelBudin <raphaelconta@hotmail.com>
2023-01-12 00:59:57 -0300

    feat/Mensagem carrinho vazio adicionada

--- src/components/cart/Total.jsx
+++ src/components/cart/Total.jsx
@@ -1,12 +1,12 @@
 import styles from "./Carrinho.module.css";
 
-export default function Total({totalPedido}) {
+export default function Total({ totalPedido }) {
+  if (!totalPedido) return (<p className={styles.naoHaItens}> Não há itens no carrinho ainda </p>);
+
   return (
     <div className={styles.linhaTotal}>
       <span>Total</span>
-      <span className={styles.totalPedido}>
-        R$ {totalPedido}
-      </span>
+      <span className={styles.totalPedido}>R$ {totalPedido}</span>
     </div>
   );
 }
